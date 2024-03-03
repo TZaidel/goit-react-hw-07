4635ac09e434cbbf893b84682092d06907450b49
TZaidel <8tzaidel@gmail.com>
2024-03-03 09:14:52 +0100

    fix fetch

--- src/ContactList/ContactList.jsx
+++ src/ContactList/ContactList.jsx
@@ -1,15 +1,23 @@
 import Contact from '../Contact/Contact.jsx';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectUsers, selectNameFilter } from '../redux/selectors.js';
+import { fetchContacts } from '../redux/operations.js';
 
 import css from './ContactList.module.css';
 
 export default function ContactList() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  console.log(selectUsers);
+
   const users = useSelector(selectUsers);
   const name = useSelector(selectNameFilter);
-  const visibleUsers = users.filter(user =>
-    user.username.toLowerCase().includes(name)
-  );
+  const visibleUsers = users.filter(user => user.name.toLowerCase().includes(name));
+  console.log(visibleUsers);
 
   return (
     <ul className={css.contactList}>
--- src/redux/usersSlice.js
+++ src/redux/usersSlice.js
@@ -3,8 +3,6 @@ import { persistReducer } from 'redux-persist';
 import { fetchContacts, addContact, deleteContact } from './operations';
 import storage from 'redux-persist/lib/storage';
 
-
-
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -14,127 +12,44 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
-
 const usersSlice = createSlice({
   name: 'users',
   initialState: {
-
-      users: [],
-      loading: false,
-      error: null,
-
+    users: [],
+    loading: false,
+    error: null,
   },
+
   extraReducers: builder =>
     builder
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.error = false
-        state.users = action.payload
+        state.isLoading = false;
+        state.error = false;
+        state.users = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.error = false
-        state.users.push(action.payload)
+        state.isLoading = false;
+        state.error = false;
+        state.users.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.error = false
-        const index = state.users.findIndex(user => user.id === action.payload.id)
-        state.users.splice(index, 1)
-      })
-//   {
-//     addUser: (state, action) => {
-//       state.contacts.users = [...state.contacts.users, action.payload];
-//     },
-
-//     deleteUser: (state, action) => {
-//       state.contacts.users = state.contacts.users.filter(user => user.id !== action.payload);
-//     },
-//   },
+        state.isLoading = false;
+        state.error = false;
+        const index = state.users.findIndex(user => user.id === action.payload.id);
+        state.users.splice(index, 1);
+      }),
 });
+
+// export const userReducer = usersSlice.reducer;
 
 const persistConfig = {
   key: 'user',
   storage,
 };
 
-// const usersSlice = createSlice({
-//   name: 'users',
-//   initialState: {
-//     contacts: {
-//       users: [],
-//       loading: false,
-//       error: null,
-//     },
-//   },
-//   reducers: {
-//     addUser: (state, action) => {
-//       state.contacts.users = [...state.contacts.users, action.payload];
-//     },
-
-//     deleteUser: (state, action) => {
-//       state.contacts.users = state.contacts.users.filter(user => user.id !== action.payload);
-//     },
-//   },
-// });
-
-// const persistConfig = {
-//   key: 'user',
-//   storage,
-// };
-
-// export const { addUser, deleteUser } = usersSlice.actions;
 export const userReducer = persistReducer(persistConfig, usersSlice.reducer);
-//!_----------------------------
-// export const addUser = createAction('users/add');
-// export const deleteUser = createAction('users/delete');
-
-// export const usersReducer = createReducer(initialState, builder =>
-//   builder
-//     .addCase(addUser, (state, action) => {
-//       return {
-//         users: [...state.users, action.payload],
-//       };
-//     })
-//     .addCase(deleteUser, (state, action) => {
-//       return {
-//         users: state.users.filter(user => user.id !== action.payload),
-//       };
-//     })
-// );
-
-// export const addUser = value => {
-//   return {
-//     type: 'users/add',
-//     payload: value,
-//   };
-// };
-
-// export const deleteUser = id => {
-//   return {
-//     type: 'users/delete',
-//     payload: id,
-//   };
-// };
-
-// export const usersReducer = (state = usersInitialState, action) => {
-//   switch (action.type) {
-//     case 'users/add':
-//       return {
-//         ...state,
-//         users: [...state.users, action.payload],
-//       };
-//     case 'users/delete':
-//       return {
-//         ...state,
-//         users: state.users.filter(user => user.id !== action.payload),
-//       };
-//     default:
-//       return state;
-//   }
-// };
